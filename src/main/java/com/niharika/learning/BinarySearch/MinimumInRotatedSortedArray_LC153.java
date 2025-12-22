63133bc02a9f5d6f78f0d021244824acac765f7d
niharikap1 <niharikapatel412@gmail.com>
2025-12-22 19:56:24 +0000

    LC 153 : Minimum in Rotated Array

--- src/main/java/com/niharika/learning/BinarySearch/MinimumInRotatedSortedArray_LC153.java
+++ src/main/java/com/niharika/learning/BinarySearch/MinimumInRotatedSortedArray_LC153.java
@@ -7,10 +7,35 @@ package com.niharika.learning.BinarySearch;
 public class MinimumInRotatedSortedArray_LC153 {
     public static void main(String[] args) {
         int[] nums = {3,4,5,1,2};
-        System.out.println(findMin(nums));
+        System.out.println(findMinOptimal(nums));
+        System.out.println(findMinBruteForce(nums));
     }
 
-    private static int findMin(int[] nums) {
+    private static int findMinBruteForce(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int num : nums){
+            if(num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    private static int findMinOptimal(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left +(right-left)/2;
+            if(mid > 0 && nums[mid] < nums[mid - 1]){
+                return nums[mid];
+            }
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
         return -1;
     }
+
 }
