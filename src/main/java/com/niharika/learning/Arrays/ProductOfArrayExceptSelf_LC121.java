2f5463a730d0786112ce969eef6922a18ee86a6f
niharikap1 <niharikapatel412@gmail.com>
2025-12-22 20:39:39 +0000

    LC 121 : Product of Array except self

--- src/main/java/com/niharika/learning/Arrays/ProductOfArrayExceptSelf_LC121.java
+++ src/main/java/com/niharika/learning/Arrays/ProductOfArrayExceptSelf_LC121.java
@@ -1,7 +1,39 @@
 package com.niharika.learning.Arrays;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf_LC121 {
     public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] result = productExceptSelfBruteForce(nums);
+        int[] resultOptimal = productExceptSelfOptimal(nums);
+       System.out.println("Result "+Arrays.toString(result));
+       System.out.println("Result "+Arrays.toString(resultOptimal));
 
     }
+
+    // Brute Force Approach - Using Division
+    //The question specify division is not allowed.However this is
+    // the brute force solution that clicks when i start solving this question
+
+    private static int[] productExceptSelfBruteForce(int[] nums) {
+        int totalProduct = 1;
+        int result[] = new int[nums.length];
+        for(int num : nums){
+            totalProduct = totalProduct * num;
+        }
+        for(int i = 0 ; i < nums.length ; i++){
+            result[i] = totalProduct / nums[i];
+        }
+
+        return result;
+    }
+
+    //TODO : Needs some revising, watch apna college video again
+    private static int[] productExceptSelfOptimal(int[] nums) {
+
+
+        return null;
+    }
+
 }
